40bf61a46f98300ac7fc88238568a5c8af2ea38b
EdimusPrime <edimusprime@Edimus-Prime.local>
2017-07-08 20:13:50 -0400

    Images and Files added from team

--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){ 
-   
+
+// $(".questions").click(function(){
+//     $('.FAQanswers').css("display", "flex");
+// });
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+$('#myBtn').click(topFunction);
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera 
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
+
+
 });
